b5fe967023e72391494c91e274cc2feead4c2401
roguib <roger.guasch.ibarra@est.fib.upc.edu>
2019-10-07 19:28:26 +0200

    Soldier animation

--- Contra/2DGame/ContraGame/ContraGame/Soldier.cpp
+++ Contra/2DGame/ContraGame/ContraGame/Soldier.cpp
@@ -6,7 +6,7 @@
 
 enum PlayerAnims
 {
-	STAND_LEFT, STAND_RIGHT, MOVE_LEFT, MOVE_RIGHT
+	STAND_LEFT, STAND_RIGHT, MOVE_LEFT, MOVE_RIGHT, LAY_DOWN
 };
 
 void Soldier::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
@@ -14,25 +14,23 @@ void Soldier::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 	bJumping = false;
 	spritesheet.loadFromFile("images/soldier.png", TEXTURE_PIXEL_FORMAT_RGBA);
 	sprite = Sprite::createSprite(glm::ivec2(32, 32), glm::vec2(0.25f, 0.25f), &spritesheet, &shaderProgram);
-	/*sprite->setNumberAnimations(4);
-
+	sprite->setNumberAnimations(10);
+	
 	sprite->setAnimationSpeed(STAND_LEFT, 8);
-	sprite->addKeyframe(STAND_LEFT, glm::vec2(0.f, 0.f));
-
-	sprite->setAnimationSpeed(STAND_RIGHT, 8);
-	sprite->addKeyframe(STAND_RIGHT, glm::vec2(0.25f, 0.f));
+	sprite->addKeyframe(STAND_LEFT, glm::vec2(0.75f, 0.25f));
 
 	sprite->setAnimationSpeed(MOVE_LEFT, 8);
 	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.f));
+	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.25f, 0.f));
+	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.5f, 0.f));
+	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.75f, 0.f));
 	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.25f));
-	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.5f));
+	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.25f, 0.5f));
 
-	sprite->setAnimationSpeed(MOVE_RIGHT, 8);
-	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.f));
-	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.25f));
-	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.5f));
+	sprite->setAnimationSpeed(LAY_DOWN, 1);
+	sprite->addKeyframe(LAY_DOWN, glm::vec2(0.25f, 0.5f));
 
-	sprite->changeAnimation(0);*/
+	sprite->changeAnimation(0);
 	tileMapDispl = tileMapPos;
 	sprite->setPosition(glm::vec2(float(tileMapDispl.x + posPlayer.x), float(tileMapDispl.y + posPlayer.y)));
 }
